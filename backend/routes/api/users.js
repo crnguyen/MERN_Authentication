4fad5272abec5daf66d8e2e5bd6f7e91ba36b3a9
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-09-03 14:16:05 -0700

    [users] add current route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -82,4 +82,13 @@ router.post('/login', (req, res) => {
   });
 });
 
+//get api/users/current (Private)
+router.get("./current", passport.authenticate("jwt", { session: false}), (req, res)=>{
+    res.json({
+        id: req.user.id,
+        name: req.user.name,
+        email: req.user.email
+    });
+});
+
 module.exports = router;
