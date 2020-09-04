14356a5f84aea8bbbf39524ffa5eba71181f1c0f
Cristina Nguyen <cristinanguyen@MacBook-Pro.local>
2020-09-04 09:49:29 -0700

    [components] add login component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,35 @@
 import React, { useEffect, useState } from 'react';
-import { Switch, Route, Redirect } from "react-router-dom";
-import jwt_decode from "jwt-decode";
-import setAuthToken from "./utils/setAuthToken";
+import { Switch, Route, Redirect } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import Navbar from './components/Navbar';
+import Signup from './components/Signup';
+import Login from './components/Login';
+// import Profile from './components/Profile';
+import Welcome from './components/Welcome';
+import About from './components/About';
+import Footer from './components/Footer';
 import './App.css';
-import Welcome from "./components/Welcome";
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
-import Profile from "./components/Profile";
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const user = localStorage.getitem("jwtToken");
-  return <Route {...rest} render={(props)=> {
-    return user ? <Component {...rest} {...props} /> : <Redirect to="/login" />
-  }}
-  />
+  const user = localStorage.getItem('jwtToken');
+  return <Route {...rest} render={(props) => {
+      return user ? <Component {...rest} {...props} /> : <Redirect to="/login" />
+    }}
+  />;
 }
 
 function App() {
+  // set state values
   let [currentUser, setCurrentUser] = useState("");
   let [isAuthenticated, setIsAuthenticated] = useState(true);
 
-  useEffect(()=>{
+  useEffect(() => {
     let token;
-    if(!localStorage.getItem("jwtToken")){
+    if (!localStorage.getItem('jwtToken')) {
       setIsAuthenticated(false);
     } else {
-      token = jwt_decode(localStorage.getItem("jwtToken"));
+      token = jwt_decode(localStorage.getItem('jwtToken'));
       setAuthToken(localStorage.jwtToken);
       setCurrentUser(token);
       setIsAuthenticated(true);
@@ -36,41 +37,41 @@ function App() {
   }, []);
 
   const nowCurrentUser = (userData) => {
-    console.log("current user is in the houuuseeeeee");
+    console.log('nowCurrentUser is working...');
     setCurrentUser(userData);
     setIsAuthenticated(true);
   };
 
   const handleLogout = () => {
-    if (localStorage.getItem("jwtToken")) {
-      localStorage.removeItem("jwtToken");
+    if (localStorage.getItem('jwtToken')) {
+      localStorage.removeItem('jwtToken');
       setCurrentUser(null);
       setIsAuthenticated(false);
     }
   }
 
-  console.log("current user:", currentUser);
-  console.log("Authenticated:", isAuthenticated);
+  console.log('Current User', currentUser);
+  console.log('Authenicated', isAuthenticated);
 
   return (
     <div>
       <Navbar handleLogout={handleLogout} isAuth={isAuthenticated} />
       <div className="container mt-5">
         <Switch>
-          {/* <Route path="/signup"/ component={Signup} /> */}
+          <Route path="/signup" component={ Signup } />
           <Route 
             path="/login" 
-            render={ (props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser}/> }
-            />
-          <Route to="/about" component={About} />
-          <PrivateRoute path="/profile" component={Profile} user={currentUser} />
-          <Route exact path="/" component={Welcome} />
+            render={ (props) => <Login {...props} setCurrentUser={setCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser}/>} 
+          />
+          <Route path="/about" component={ About } />
+          {/* <PrivateRoute path="/profile" component={ Profile } user={currentUser} /> */}
+          <Route exact path="/" component={ Welcome } />
         </Switch>
       </div>
-      {/* <Welcome /> */}
       <Footer />
     </div>
   );
 }
 
 export default App;
+
